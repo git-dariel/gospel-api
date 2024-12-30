b3fcc9914250fa16c3a1d5ad680fa16b67787793
git-dariel <104293978+git-dariel@users.noreply.github.com>
2024-12-30 22:10:12 +0800

    initial commit

---
+++ config/constants.ts
@@ -0,0 +1,16 @@
+// Purpose: To store all the constants used in the application.
+export const config = {
+  PORT: 5000,
+
+  MESSAGE: {
+    WELCOME: "You're successfully connected to MONGO API.",
+  },
+
+  DB: {
+    // Create you own mongodb URI from Atlas MongoDB and use it here.
+    URI: "mongodb+srv://myAtlasDBUser:database_2025@myatlasclusteredu.4pyi0.mongodb.net/?retryWrites=true&w=majority&appName=myAtlasClusterEDU",
+    ERROR: "Error connecting to database: ",
+    NOT_INITIALIZED: "Database connection not initialized",
+    CONNECTED: "Connected to database",
+  },
+};
